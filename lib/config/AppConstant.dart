31f3cd296e830fa41bd7de3aae137b009c36aa36
Devendra <ankit2004dewangan@gmail.com>
2026-02-15 23:27:54 +0530

    Removed Colors and Constant Defining it using manually

--- lib/config/AppConstant.dart
+++
@@ -1,4 +0,0 @@
-class AppConstant {
-  static const String inter = 'Inter';
-  static const String interBold = 'Inter';
-}
--- lib/config/ScreenSize.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScreenSize {
-  static bool isMobile(BuildContext context) {
-    return MediaQuery.of(context).size.width < 650;
-  }
-}
